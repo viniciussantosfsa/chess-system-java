5ca3ec0ec071e90cdf8ae52754d089d3e8e17eab
Viníciusfsa <viniciussantos123fsa@gmail.com>
2023-08-10 19:27:04 -0300

    add boardException

--- src/main/java/com/br/me/boardgame/Board.java
+++ src/main/java/com/br/me/boardgame/Board.java
@@ -1,10 +1,14 @@
 package com.br.me.boardgame;
+
 public class Board {
     private int rows;
     private int columms;
     private Piece[][] pieces;
 
     public Board(int rows, int columms) {
+        if (rows < 1 || columms < 1) {
+            throw new BoardException("Error creating board: there must be at least 1 row and 1 columm");
+        }
         this.rows = rows;
         this.columms = columms;
         pieces = new Piece[rows][columms];
@@ -14,28 +18,45 @@ public class Board {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
     public int getColumms() {
         return columms;
     }
 
-    public void setColumms(int columms) {
-        this.columms = columms;
-    }
-
     public Piece piece(int row, int columm) {
+        if (!positionExists(row, columm)) {
+            throw new BoardException("Position not on the board");
+        }
         return pieces[row][columm];
     }
 
     public Piece piece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position not on the board");
+        }
         return pieces[position.getRow()][position.getColumm()];
     }
 
     public void placePiece(Piece piece, Position position) {
+        if (thereIsAPiece(position)) {
+            throw new BoardException("There is already a piece on position " + position);
+        }
         pieces[position.getRow()][position.getColumm()] = piece;
         piece.position = position;
     }
+
+    private boolean positionExists(int row, int columm) {
+        return row >= 0 && row < rows && columm >= 0 && columm < columms;
+    }
+
+    public boolean positionExists(Position position) {
+        return positionExists(position.getRow(), position.getColumm());
+    }
+
+    public boolean thereIsAPiece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position not on the board");
+        }
+        return piece(position) != null;
+    }
+
 }
---
+++ src/main/java/com/br/me/boardgame/BoardException.java
@@ -0,0 +1,7 @@
+package com.br.me.boardgame;
+
+public class BoardException extends RuntimeException {
+    public BoardException(String msg) {
+        super(msg);
+    }
+}
